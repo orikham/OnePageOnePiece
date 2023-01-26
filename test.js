a86c6bbcebd21f8890198b925adb70e4ab8b91de
orikham <orikham@hotmail.fr>
2023-01-26 13:54:14 +0100

    1er commit

---
+++ test.js
@@ -0,0 +1,27 @@
+let topArrow = document.getElementById('top-arrow');
+let bottomArrow = document.getElementById('bottom-arrow');
+let recettes = document.getElementById('recettes');
+let transition = 0;
+
+
+topArrow.addEventListener('click', function(){
+    if(transition - 80 >= 2000){
+        transition -= 80;
+
+    recettes.style.marginTop = transition + 'px';
+
+
+    
+}
+});
+
+bottomArrow.addEventListener('click', function(){
+    if(transition + 80 <= 2000){
+        transition += 80;
+
+    recettes.style.marginTop = transition + 'px';
+
+
+    
+}
+});
